caaa17d4c9e42ae579afd50f7d78dda34f96ef13
= <ab.iom@yandex.com>
2022-07-20 01:58:56 +0300

    one more rev while studying

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,14 +1,16 @@
 # kittygram_plus/urls.py
 from rest_framework.routers import DefaultRouter
-
 from django.urls import include, path
+from cats.views import CatViewSet, LightCatViewSet, OwnerViewSet
 
-from cats.views import CatViewSet, OwnerViewSet
+
+
 
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
 router.register('owners', OwnerViewSet)
+router.register(r'mycats', LightCatViewSet) 
 
 urlpatterns = [
     path('', include(router.urls)),
